5432b91cd5f0544fa66a1cfc29657dca102e97a1
chouchlias <chouchlias@netlink.gr>
2023-10-04 16:40:30 +0300

    Updates

---
+++ precache-manifest.463cc3d2a634291e0bb0f55f5cf06586.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "889e6344e43e28172c2f",
+    "url": "/Portfolio/static/css/main.8e7531d3.chunk.css"
+  },
+  {
+    "revision": "889e6344e43e28172c2f",
+    "url": "/Portfolio/static/js/main.889e6344.chunk.js"
+  },
+  {
+    "revision": "458c02bb6573f6991b75",
+    "url": "/Portfolio/static/js/1.458c02bb.chunk.js"
+  },
+  {
+    "revision": "3b96116072eb85f5a3a2",
+    "url": "/Portfolio/static/js/runtime~main.3b961160.js"
+  },
+  {
+    "revision": "ff1b1ecdabe639635d3537306d41fed7",
+    "url": "/Portfolio/index.html"
+  }
+];
